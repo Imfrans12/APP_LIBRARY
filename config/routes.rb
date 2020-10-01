f7b8936073700ff0050e1a248ee0c61410ae48d6
Imfrans <fransiscoleonardogabriel@smkwikrama.sch.id>
2020-10-01 18:24:36 +0700

    Second Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :ms_users
   resources :ms_members
   resources :ms_books
   resources :tr_loans
   resources :ms_publishers
-  
+
+  root "ms_members#index"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
